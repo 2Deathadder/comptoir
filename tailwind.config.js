d74e82bc29cc6fc6f1fedd1523944f9e3f7da4cf
2Deathadder <3thomwinner2007@gmail.com>
2026-08-09 23:46:38 +0000

    Ajout/MAJ via génération IA 21st.dev : tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content: ['./index.html','./src/**/*.{js,jsx}'], darkMode: 'class', theme: { extend: { colors: { ink:'#18231f', moss:'#496052', sage:'#e1e9df', coral:'#e7785d', sand:'#f7f5ef', gold:'#d5a84b' }, fontFamily:{ sans:['Inter','ui-sans-serif','system-ui'], display:['DM Sans','Inter','sans-serif'] } } }, plugins: [] }
